0b009dca423732919153aa9d04dc202e737e1c28
Sermad Ahmad <71033@gcslahore.edu.pk>
2024-08-04 22:00:02 +0500

    feat: add keyboard action buttons to tip calculator

--- app/src/main/java/com/example/customtipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/customtipcalculator/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -21,7 +22,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -56,7 +62,7 @@ fun CustomTipCalculatorScreen(){
 @Composable
 fun Title() {
     Text(
-        text = "Custom Tip Calculator",
+        text = stringResource(R.string.custom_tip_calculator),
         fontSize = 24.sp,
         fontWeight = FontWeight.Bold,
         textAlign = TextAlign.Center,
@@ -67,27 +73,29 @@ fun Title() {
 }
 
 @Composable
-fun BillAmountInput(billAmount: String, onBillAmountChange: (String) -> Unit){
+fun BillAmountInput(billAmount: String, focusManager: FocusManager, onBillAmountChange: (String) -> Unit){
     TextField(
         modifier = Modifier
             .padding(20.dp)
             .fillMaxWidth(),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
+        keyboardActions = KeyboardActions(onNext = {focusManager.moveFocus(FocusDirection.Down) }),
         value = billAmount,
-        label = {Text(text = "Enter Bill Amount")},
+        label = {Text(text = stringResource(R.string.enter_bill_amount))},
         onValueChange = onBillAmountChange
     )
 }
 
 @Composable
-fun TipPercentageInput(tipPercentage: String, onTipPercentageChange: (String) -> Unit){
+fun TipPercentageInput(tipPercentage: String, focusManager: FocusManager, onTipPercentageChange: (String) -> Unit){
     TextField(
         modifier = Modifier
             .padding(horizontal = 20.dp)
             .fillMaxWidth(),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onNext = {focusManager.clearFocus() }),
         value = tipPercentage,
-        label = {Text(text = "Tip (%)")},
+        label = {Text(text = stringResource(R.string.tip))},
         onValueChange = onTipPercentageChange
     )
 }
@@ -101,7 +109,7 @@ fun RoundUpSwitch(roundUp: Boolean, onRoundUpSwitchChange: (Boolean) -> Unit){
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
-        Text(text = "Round Up Tip?", textAlign = TextAlign.Start)
+        Text(text = stringResource(R.string.round_up_tip), textAlign = TextAlign.Start)
         Switch(
             checked = roundUp,
             onCheckedChange = onRoundUpSwitchChange
@@ -112,7 +120,7 @@ fun RoundUpSwitch(roundUp: Boolean, onRoundUpSwitchChange: (Boolean) -> Unit){
 @Composable
 fun TipAmountDisplay(tipAmount: String){
     Text(
-        text = "Tip Amount: $$tipAmount",
+        text = stringResource(R.string.tip_amount) + tipAmount,
         fontSize = 24.sp,
         fontWeight = FontWeight.SemiBold,
         textAlign = TextAlign.Center,
@@ -139,15 +147,16 @@ fun CustomTipCalculator() {
     var tipPercentage by remember { mutableStateOf("") }
     var roundUp by remember { mutableStateOf(true) }
     var tipAmount by remember { mutableStateOf("0.00") }
+    val focusManager = LocalFocusManager.current
 
     Column {
         Title()
-        BillAmountInput(billAmount){
+        BillAmountInput(billAmount, focusManager){
             if (it.isDigitsOnly()) {
                 billAmount = it
             }
         }
-        TipPercentageInput(tipPercentage){
+        TipPercentageInput(tipPercentage, focusManager){
             if (it.isDigitsOnly()) {
                 tipPercentage = it
             }
